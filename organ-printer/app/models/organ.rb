8eb84c196ae823c67c92301244611473f9a65cd1
github cjl248 <github cjl248@cornell.edu>
2019-08-21 18:07:36 -0400

    user-balance-updating-correctly

--- organ-printer/app/models/organ.rb
+++ organ-printer/app/models/organ.rb
@@ -9,7 +9,7 @@ class Organ < ApplicationRecord
 
   def random_price
     #want it to be between 20% and 400% of the cost o build
-    self.cost_to_build * rand(0.1..4.0)
+    self.cost_to_build * rand(0.1..4.0).round(2)
   end
 
 end
--- organ-printer/config/routes.rb
+++ organ-printer/config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   end
   resources :user_organs, only: [:show, :destroy]
 
+  ## CUSTOM ROUTE FOR STORING A USER_ORGAN ##
+  put '/store', to: 'users#store'
+  ## CUSTOM ROUTE FOR CHECKING MARKET PRICE ##
+  put "/user_organs/:id/check", to: 'user_organs#check'
+
   ## REGISTER ##
   get '/register', to: 'users#new'
   post '/register', to: 'users#create'
@@ -12,5 +17,7 @@ Rails.application.routes.draw do
   get "/login", to: 'login#new'
   post "/login", to: 'login#create'
   delete '/logout', to: 'login#destroy'
-  
+
+  # root ''
+
 end
